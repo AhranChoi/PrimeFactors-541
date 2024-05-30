5ae965d6731b63bb3dfd3234c7dc3eff4392ea79
ahran_.choi <carokq@gmail.com>
2024-05-30 11:43:14 +0900

    [feature] 2
    (cherry picked from commit 161a126fc8beb71206eb5d366f9d10f41edebeab)

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -8,3 +8,9 @@ TEST(PrimeFactors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(primeFactor.of(1), expected);
 }
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor primeFactor;
+	vector<int> expected = { 2 };
+	EXPECT_EQ(primeFactor.of(2), expected);
+}
